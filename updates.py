a21c7addfcc58f58e76ac5c25c6385214d60c101
Pascale Möller <pinnelot@hotmail.de>
2014-05-28 18:24:10 +0200

    weitere Optimierung und absolute Fertigstellung der Hauptfunktionalität

--- updates.py
+++ updates.py
@@ -13,7 +13,6 @@ tmpHTMLFILE_EN = "Contents/tmpELBW.html"
 RSSFILE_DE = "de.rss"
 RSSFILE_EN = "en.rss"
 
-debug = True
 
 def getVer(path):
     """Liest alle Dateinamen in Support/RawBin/ , um die aktuellste
@@ -35,9 +34,9 @@ def updateVer(Ver):
     """Ändere die angezeigte Version in RSS und HTML
     """
     setHTMLVer(Ver, "de")
-    #setHTMLVer(Ver, "en")
-    #setRSSVer(Ver, "de")
-    #setRSSVer(Ver, "en")
+    setHTMLVer(Ver, "en")
+    setRSSVer(Ver, "de")
+    setRSSVer(Ver, "en")
 
 def setHTMLVer(Ver, lang):
     """Andert die angezeigte Version in HTML
@@ -47,108 +46,39 @@ def setHTMLVer(Ver, lang):
     formatDate = "%i.%i.%i" %(verTime.tm_mday, verTime.tm_mon, verTime.tm_year)
     
     if (lang == "de"):
-        file_de = open(tmpHTMLFILE_DE, "w")
-        dom_file = open(HTMLFILE_DE, encoding='utf-8')
-        dom = xml.dom.minidom.parse(dom_file)
-        #RSS root node
-        root = dom.documentElement
-        for article in root.getElementsByTagName("article"):
-            if (article.hasAttribute("id")):
-                article0 = article
-                
-        timedate = article0.getElementsByTagName("time")[0]
-        timedate.setAttribute("datetime", formatDatetime)
-        timedate.firstChild.replaceWholeText(formatDate)
-        
-        header = article0.getElementsByTagName("h1")[0]
-        header.firstChild.replaceWholeText("Aktuelle LOCKBASE Version: %0.2f" % (Ver,))
-
-        paragraph = article0.getElementsByTagName("p")[0]
-        paragraph.firstChild.replaceWholeText("Die aktuelle LOCKBASE Version %0.2f steht ab jetzt für Sie zum Update bereit." % (Ver,))
-        
-        root.writexml(file_de)
-
-
-def old_setHTMLVer(Ver, lang):
-    """Ändert die angezeigte Version in HTML
-    """
-    verTime = time.gmtime(os.path.getmtime(PATH + str(Ver) + ".txt.bak"))
-    formatDatetime = "%i-%i-%i %i:%i" % (verTime.tm_year, verTime.tm_mon, verTime.tm_mday, verTime.tm_hour, verTime.tm_min)
-    formatDate = "%i.%i.%i" %(verTime.tm_mday, verTime.tm_mon, verTime.tm_year)
-
-    if (lang == "de"):
-        infile = open(HTMLFILE_DE, 'r')
-        outfile = open(tmpHTMLFILE_DE, 'w')
-        tmpLine = infile.readline()
-        while ("<article id=\"version\">" not in tmpLine):
-            outfile.write(tmpLine)
-            tmpLine = infile.readline()
-        outfile.write("""<article id="version">
-						<footer>
-						  <time datetime="%s">
-						    %s
-						  </time>
-						</footer>
-						<header>
-							<h1>
-							  Aktuelle LOCKBASE Version: %0.2f
-							</h1>
-						</header>
-						<p>
-						  Die aktuelle LOCKBASE Version %0.2f steht ab jetzt für Sie zum Update bereit. 
-						    <span class="more">
-						      <a href="@NAVI=GSupport">
-							mehr
-						      </a>
-						    </span>
-						</p>
-					</article>""" % (formatDatetime, formatDate, Ver, Ver))
-                      
-        while ("</article>" not in tmpLine):
-                      tmpLine = infile.readline()
-                      
-        for line in infile:
-            outfile.write(line)
-        infile.close()
-        outfile.close()
-        #os.rename(tmpHTMLFILE_DE, HTMLFILE_DE)
+        srcHTML = HTMLFILE_DE
+        headertxt = "Aktuelle LOCKBASE Version: %0.2f" % (Ver,)
+        ptxt = "Die aktuelle LOCKBASE Version %0.2f steht ab jetzt für Sie zum Update bereit." % (Ver,)
     elif (lang == "en"):
-        infile = open(HTMLFILE_EN, 'r')
-        outfile = open(tmpHTMLFILE_EN, 'w')
-        tmpLine = infile.readline()
-        while ("<article id=\"version\">" not in tmpLine):
-            outfile.write(tmpLine)
-            tmpLine = infile.readline()
-        outfile.write("""<article id="version">
-						<footer>
-							<time datetime="%s">
-								%s
-							</time>
-						</footer>
-						<header>
-							<h1>
-								Latest LOCKBASE version:  %0.2f
-							</h1>
-						</header>
-						<p>
-							The latest LOCKBASE version is %0.2f and can be downloaded  			
-							<span class="more">
-							<a href="@NAVI=GSupport">
-								here
-							</a>
-						</span>
-						</p>
-					</article>""" % (formatDatetime, formatDate, Ver, Ver))
-                      
-        while ("</article>" not in tmpLine):
-                      tmpLine = infile.readline()
-                      
-        for line in infile:
-            outfile.write(line)
-        infile.close()
-        outfile.close()
-        #os.rename(tmpHTMLFILE_EN, HTMLFILE_EN)
+        srcHTML = HTMLFILE_EN
+        headertxt = "Latest LOCKBASE version:  %0.2f" % (Ver,)
+        ptxt = "The latest LOCKBASE version is %0.2f and can be downloaded " % (Ver,)
+
         
+    #file = open(os.path.join(os.path.dirname(srcHTML),"tmp" + os.path.basename(srcHTML)), "w")
+    dom_file = open(srcHTML, encoding='utf-8')
+    dom = xml.dom.minidom.parse(dom_file)
+    #RSS root node
+    root = dom.documentElement
+    for article in root.getElementsByTagName("article"):
+        if (article.hasAttribute("id")):
+            article0 = article
+            break
+
+    for node in article0.childNodes:
+        if node.nodeName == "footer":
+            timedate = node.getElementsByTagName("time")[0]
+            timedate.setAttribute("datetime", formatDatetime)
+            timedate.firstChild.replaceWholeText(formatDate)
+        elif node.nodeName == "header":
+            header = article0.getElementsByTagName("h1")[0]
+            header.firstChild.replaceWholeText(headertxt)
+        elif node.nodeName == "p":
+            paragraph = article0.getElementsByTagName("p")[0]
+            paragraph.firstChild.replaceWholeText(ptxt)
+
+    file = open(srcHTML, "w")       
+    root.writexml(file)
         
 
 def setRSSVer(Ver, lang):
@@ -159,47 +89,38 @@ def setRSSVer(Ver, lang):
     pubDateFormat = datetime.datetime.fromtimestamp(ts).strftime('%a, %d %b %Y %H:%M:%S')
 
     if (lang == "de"):
-        #TODO: Edit file_de to path RSSFILE_DE (without tmp)
-        file_de = open("tmp" + RSSFILE_DE, "w")
-        dom = xml.dom.minidom.parse(RSSFILE_DE)
-        #RSS root node
-        root = dom.documentElement
-        channel = root.childNodes[1]
-        lastBDate = channel.getElementsByTagName("lastBuildDate")[0]
-        verItem = channel.getElementsByTagName("item")[0]
-        title = verItem.getElementsByTagName("title")[0].firstChild
-        title.replaceWholeText("Aktuelle LOCKBASE Version: %0.2f" % (Ver,))
-        pubDate = verItem.getElementsByTagName("pubDate")[0].firstChild
-        pubDate.replaceWholeText(pubDateFormat)
-        descr = verItem.getElementsByTagName("description")[0].firstChild
-        descr.replaceWholeText("<p>Die aktuelle LOCKBASE Version %0.2f steht ab jetzt für Sie zum Update bereit.</p>" % (Ver,))
-        print(descr)
-        print(descr.nodeValue)
-        print(verItem)
-        root.writexml(file_de)
-
+        srcRSS = RSSFILE_DE
+        titletxt = "Aktuelle LOCKBASE Version: %0.2f" % (Ver,)
+        descrtxt = "<p>Die aktuelle LOCKBASE Version %0.2f steht ab jetzt für Sie zum Update bereit.</p>" % (Ver,)
     elif (lang == "en"):
-        #TODO: Edit file_de to path RSSFILE_EN (without tmp)
-        file_de = open("tmp" + RSSFILE_EN, "w")
-        dom = xml.dom.minidom.parse(RSSFILE_EN)
-        #RSS root node
-        root = dom.documentElement
-        channel = root.childNodes[1]
-        lastBDate = channel.getElementsByTagName("lastBuildDate")[0]
-        verItem = channel.getElementsByTagName("item")[0]
-        title = verItem.getElementsByTagName("title")[0].firstChild
-        title.replaceWholeText("Latest LOCKBASE Version: %0.2f" % (Ver,))
-        pubDate = verItem.getElementsByTagName("pubDate")[0].firstChild
-        pubDate.replaceWholeText(pubDateFormat)
-        descr = verItem.getElementsByTagName("description")[0].firstChild
-        descr.replaceWholeText("<p>The latest LOCKBASE Version %0.2f can be downloaded.</p>" % (Ver,))
-        print(descr)
-        print(descr.nodeValue)
-        print(verItem)
-        root.writexml(file_de)
+        srcRSS = RSSFILE_EN
+        titletxt = "Latest LOCKBASE Version: %0.2f" % (Ver,)
+        descrtxt = "<p>The latest LOCKBASE Version %0.2f can be downloaded.</p>" % (Ver,)
+    
+
+    dom = xml.dom.minidom.parse(srcRSS)
+    #RSS root node
+    root = dom.documentElement
+    channel = root.childNodes[1]
+
+    for node in channel.childNodes:
+        if (node.nodeName == "lastBDate"):
+            print("lala")
+        elif (node.nodeName == "item"):
+            for chNode in node.childNodes:
+                if (chNode.nodeName == "title"):
+                    chNode.firstChild.replaceWholeText(titletxt)
+                elif (chNode.nodeName == "pubDate"):
+                    chNode.firstChild.replaceWholeText(pubDateFormat)
+                elif (chNode.nodeName == "description"):
+                    chNode.firstChild.replaceWholeText(descrtxt)
+
+    file = open(srcRSS, "w")
+    root.writexml(file)
+
     
 
 
-#current_ver = getVer(PATH)
-current_ver = 66.666
+current_ver = getVer(PATH)
+#current_ver = 66.666
 updateVer(current_ver)
